e1934f6f77069d4036925adefa168d4f40a32c91
1245193880 <1245193880@qq.com>
2016-12-06 08:45:26 +0800

    te

--- src/test/java/com/a/www/dao/test/TestHello.java
+++ src/test/java/com/a/www/dao/test/TestHello.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 import com.a.www.dao.Hello;
 
 /**
- *@author:12
- *@Time:2016年12月6日上午8:43:31
- *@Description:TODO
- *@FileInfo:com.a.www.dao.test.TestHello.java
+ * @author:12
+ * @Time:2016年12月6日上午8:43:31
+ * @Description:TODO
+ * @FileInfo:com.a.www.dao.test.TestHello.java
  */
 public class TestHello extends AbstractHello {
-	
+
 	@Test
 	public void testHello() {
 		Hello h = new Hello();
